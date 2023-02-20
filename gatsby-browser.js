d791f449c44b8252db7bb941cf86c310758d480c
Dmitriy Shcherbakan <some.nugget@gmail.com>
2023-02-20 13:17:35 +0300

    test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,11 @@ require('./src/styles/styles.scss')
 
 const highlight = require('highlight.js')
 
+exports.onPreRouteUpdate = ({ location, prevLocation }) => {
+  console.log(document.querySelectorAll('code[class]'))
+  highlight.highlightAll()
+}
+
 exports.onRouteUpdate = () => {
   highlight.highlightAll()
 }
